c033507818e8a7cf22d9afc63ae9a0c75b3a7fc7
Trestan Pillonel <trestan.pillonel@gmail.com>
2015-04-01 15:26:23 +0200

    refactoring

--- eutils/genbank2refseq.py
+++ eutils/genbank2refseq.py
@@ -13,14 +13,14 @@ def gi(ncbi_term, database="nuccore"):
     return record["IdList"]
 
 
-def genbank2refseq(ncbi_id, database=nuccore):
+def genbank2refseq(ncbi_id, database="nuccore"):
     '''
     :param genbank id
     :return: refseq_id
     '''
 
     # get link from genbank 2 refseq
-    handle = Entrez.elink(dbfrom="nuccore", db="nuccore", id=gi(ncbi_id), term="srcdb+refseq[prop]")
+    handle = Entrez.elink(dbfrom="nuccore", db=database, id=gi(ncbi_id), term="srcdb+refseq[prop]")
     record = Entrez.read(handle)
 
     try:
@@ -28,24 +28,24 @@ def genbank2refseq(ncbi_id, database=nuccore):
         refseq_id = record[0]['LinkSetDb'][0]['Link'][0]['Id'] #['IdList']
     except IndexError:
         return None
-    ref_seq_accession = Entrez.efetch(db="nucleotide", id=refseq_id, rettype="acc")
+    ref_seq_accession = Entrez.efetch(db=database, id=refseq_id, rettype="acc")
 
     return ref_seq_accession.read()
 
 
-def refseq2genbank(ncbi_id):
+def refseq2genbank(ncbi_id, database="nuccore"):
     '''
     :param refseq_id
     :return: genbank id
     '''
 
-    handle = Entrez.elink(dbfrom="nuccore", db="nuccore", id=gi(ncbi_id), term="srcdb+ddbj/embl/genbank[prop]")
+    handle = Entrez.elink(dbfrom=database, db=database, id=gi(ncbi_id), term="srcdb+ddbj/embl/genbank[prop]")
     record = Entrez.read(handle)
     try:
         genbank_id = record[0]['LinkSetDb'][0]['Link'][0]['Id'] #['IdList']
     except IndexError:
         return None
-    genbank_accession = Entrez.efetch(db="nucleotide", id=genbank_id, rettype="acc")
+    genbank_accession = Entrez.efetch(db=database, id=genbank_id, rettype="acc")
 
     return genbank_accession.read()
 
@@ -67,17 +67,19 @@ def identify_id(ncbi_id, database = "nuccore"):
 
 if __name__ == '__main__':
     import argparse
+    import sys
+    from Bio import SeqIO
     parser = argparse.ArgumentParser()
     parser.add_argument("-g",'--seq_id_genbank', default=False, type=str, help="genbank2refseq")
     parser.add_argument("-r",'--seq_id_refseq', default=False, type=str, help="refseq2genbank")
     parser.add_argument("-i",'--info', default=False, type=str, help="Get search result from input identifier")
-    parser.add_argument("-i",'--database', default="nuccore", type=str, help="Target database (genome, protein, nucccore, bioproject,...")
+    parser.add_argument("-d",'--database', default="nuccore", type=str, help="Target database (genome, protein, nucccore, bioproject,...")
 
     args = parser.parse_args()
 
     if args.seq_id_genbank:
-        print genbank2refseq(args.seq_id_genbank, args.database)
+        sys.stdout.write(genbank2refseq(args.seq_id_genbank, args.database))
     if args.seq_id_refseq:
-        print refseq2genbank(args.seq_id_refseq, args.database)
+        sys.stdout.write(refseq2genbank(args.seq_id_refseq, args.database))
     if args.info:
-        print identify_id(args.seq_id_refseq, args.database)
+        SeqIO.write(identify_id(args.seq_id_refseq, args.database), sys.stdout, "genbank")
