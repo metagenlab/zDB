d9f1f429768aee3f9a9ce9a1451c6e33d3ed88ec
Trestan Pillonel <trestan.pillonel@gmail.com>
2015-04-13 16:06:54 +0200

    refactoring

--- chlamdb/views.py
+++ chlamdb/views.py
@@ -198,7 +198,8 @@ def circos_homology(request, biodb):
                 else:
                     record_list.append(biorecord)
 
-            circos_orthology.circos_orthology(record_list, ref_record[1:], target_records)
+            path = os.path.join(settings.BASE_DIR, "/assets/circos")
+            circos_orthology.circos_orthology(record_list, ref_record[1:], target_records, location = path)
             circos_file = "circos/circos.svg"
             envoi_circos = True
 
