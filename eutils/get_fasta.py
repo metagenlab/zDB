ed8d1354dc071b4a488a6f27f4ac710ba438506b
cbertell <claire.bertelli@gmail.com>
2015-04-22 18:42:18 +0200

    blast_ncbi.py and get_fasta.py: minor changes

--- eutils/get_fasta.py
+++ eutils/get_fasta.py
@@ -1,36 +1,53 @@
 #!/usr/bin/env python
 
-# get a set of nucleotide or protein sequences from ncbi using eutils
+# get a set of nucleotide or protein sequences from ncbi using Entrez
 # Author: Claire Bertelli (claire.bertelli[@]gmail.com)
 # Date: 04.2015
 # ---------------------------------------------------------------------------
 
 import argparse
-from Bio import Entrez, SeqIO
+from Bio import Entrez
 import sys
 from pandas import read_table
+import re
+from shell_command import shell_command
 
 Entrez.email = "claire.bertelli@chuv.ch"
 
-def download_seq_entrez(accession, multifasta, seq_type):
+def download_seq_entrez(accession, multifasta, seq_type, append):
     # retrieves information about entry in ncbi using Entrez
 
 
     # if the user requests individual fasta file per accession
     if multifasta=="":
         for one_accession in accession:
-            output_handle = open("%s.fasta" %one_accession, "w")
+            output_name = re.sub("([a-zA-Z_0-9]+)\.([a-zA-Z]+)","\\1.fasta",one_accession)
+            output_handle = open(output_name, "w")
             handle = Entrez.efetch(db=seq_type, id=one_accession, rettype="fasta")
             output_handle.write(handle.read())
             output_handle.close()
 
      # if the user requests a single multifasta file
     else:
-        output_handle = open("%s.fasta" %multifasta, "w")
-        handle = Entrez.efetch(db=seq_type, id=accession, rettype="fasta")
-        output_handle.write(handle.read())
+        # if the user required to add the blast file
+        if append != "":
+            output_handle = open("%s_only.fasta" %multifasta, "w")
+            handle = Entrez.efetch(db=seq_type, id=accession, rettype="fasta")
+            output_handle.write(handle.read())
+            output_handle.close()
 
-        output_handle.close()
+            cmd = "cat %s %s_only.fasta > %s.fasta" % (append, multifasta, multifasta)
+            print cmd
+            shell_command(cmd)
+            cmd = "rm %s_only.fasta" % multifasta
+            print cmd
+            shell_command(cmd)
+
+        else:
+            output_handle = open("%s.fasta" %multifasta, "w")
+            handle = Entrez.efetch(db=seq_type, id=accession, rettype="fasta")
+            output_handle.write(handle.read())
+            output_handle.close()
 
 
 # reads table and retrieve GI in the column specified by the user
@@ -43,10 +60,11 @@ def table_to_gi(file, column):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-a",'--accession',type=str,  help="file with accession numbers")
+    parser.add_argument("-a",'--accession',type=str,  help="File with accession numbers")
     parser.add_argument("-t",'--seq_type',type=str, default="protein", help="Type of sequence to be downloaded: protein, gene, nuccore. Default: protein")
     parser.add_argument("-m",'--multifasta',type=str, default="", help="For multiple accession numbers, enable to download all sequences in a multifasta file. Please provide a prefix for the output.")
     parser.add_argument("-c",'--column',type=int, default="", help="Input is a file containing a tabulated table containing the GI of sequences to retrieve in column c. Please specify column number.")
+    parser.add_argument("-p", '--append', type=str, default="", help="If required, name of the blast query file that should be appended to the output fasta file")
 
     args = parser.parse_args()
 
@@ -56,4 +74,4 @@ if __name__ == '__main__':
         accession=list(args.accession)
 
     sys.stdout.write("getting protein sequences %s ... \n" % accession)
-    download_seq_entrez(accession, args.multifasta, args.seq_type)
+    download_seq_entrez(accession, args.multifasta, args.seq_type, args.append)
