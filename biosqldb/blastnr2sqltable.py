04ee1274e00a1b8a8a3f8c594a803fd3be0a8c17
Trestan Pillonel <trestan.pillonel@gmail.com>
2015-05-11 19:08:45 +0200

    -

--- biosqldb/blastnr2sqltable.py
+++ biosqldb/blastnr2sqltable.py
@@ -759,11 +759,12 @@ def del_blastnr_table_content(db_name):
     all_accessions = [i[0] for i in server.adaptor.execute_and_fetchall(sql,)]
 
     for accession in all_accessions:
-        sql1 = 'delete from blastnr.blastnr_hsps_%s_%s' % (db_name, accession)
-        sql2 = 'delete from blastnr.blastnr_hits_%s_%s' % (db_name, accession)
-        sql3 = 'delete from blastnr.blastnr_hits_taxonomy_%s_%s' % (db_name, accession)
-        server.adaptor.execute(sql1)
+        sql1 = 'DROP TABLE blastnr.blastnr_hsps_%s_%s' % (db_name, accession)
+        sql2 = 'DROP TABLE blastnr.blastnr_hits_%s_%s' % (db_name, accession)
+        sql3 = 'DROP TABLE blastnr.blastnr_hits_taxonomy_%s_%s' % (db_name, accession)
+
         server.adaptor.execute(sql2)
+        server.adaptor.execute(sql1)
         server.adaptor.execute(sql3)
 
 
