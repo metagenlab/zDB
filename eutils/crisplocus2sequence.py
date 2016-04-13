9c30a542e2d6a5f92587c441cb1aedd98a8c0177
Trestan Pillonel <trestan.pillonel@gmail.com>
2016-04-13 15:56:41 +0200

    crisp download

---
+++ eutils/crisplocus2sequence.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python
+
+from Bio import Entrez, SeqIO
+
+Entrez.email = "trestan.pillonel@unil.ch"
+import gbk2ffn
+import gbk2faa
+import urllib2
+import genbank2refseq
+from Bio.Alphabet import generic_nucleotide
+from Bio.Alphabet import generic_dna
+def chunks(l, n):
+    "return sublists of l of minimum length n (work subdivision for the subprocesing module"
+    return [l[i:i+n] for i in range(0, len(l), n)]
+
+
+def get_protein_sequences(crispdata):
+
+
+    import time
+    from Bio.SeqRecord import SeqRecord
+
+    mychunks = chunks(crispdata.keys(), 10)
+
+    for i, chunk in enumerate(mychunks):
+        print "%s/%s" % (i, len(mychunks))
+        table=1
+
+        print chunk
+        i = 0
+        handle = None
+        while not handle:
+            print i
+            if i == 10:
+                print 'reached max iteration number, %s could not be downloaded' % chunk
+                break
+            try:
+                handle = Entrez.efetch(db="nucleotide", id=','.join(chunk), rettype="fasta", retmode="text")
+            except (urllib2.URLError, urllib2.HTTPError) as e:
+                print e
+                print 'url error, trying again...'
+                time.sleep(1)
+                i+=1
+        if not handle:
+            continue
+
+        seq_records = list(SeqIO.parse(handle, "fasta"))
+
+        for record in seq_records:
+            ncbi_accession = record.accession.split('.')[0]
+            print 'accession', ncbi_accession
+            for gene_data in crispdata[ncbi_accession]:
+
+                localisation = gene_data[0].split('..')
+                strand = gene_data[1]
+                start = int(localisation[0])-1
+                end = int(localisation[1])
+                gene_name = ncbi_accession + '_' + gene_data[2]
+
+                #print seq_record[start:end].seq
+
+                if strand == '-':
+                    print ncbi_accession, 'strand -', gene_name, start, end, table, seq_record.description
+                    if "Mycoplasma" in seq_record.description:
+                        print 'Myco!'
+                        table=4
+                    seq = seq_record[start:end].seq.reverse_complement().translate(table=table)
+                    if '*' in seq[0:-1] and table ==1:
+                        print 'problem with translation, trying to identitfy translation table'
+                        handle = Entrez.efetch(db="nucleotide", id=ncbi_accession, rettype="gbwithparts", retmode="text")
+                        seq_record = SeqIO.read(handle, "genbank")
+                        for feature in seq_record.features:
+                            if feature.type == 'CDS':
+                                table= int(feature.qualifiers["transl_table"][0])
+                        seq = seq_record[start:end].seq.reverse_complement().translate(table=table)
+                        print ncbi_accession, 'strand -', gene_name, start, end, table, seq_record.description
+                        #print seq
+
+                    if '*' in seq[0:-1]:
+                        print 'changing start and stop: %s %s' % (start+1, end+1)
+                        seq = seq_record[(start+1):(end+1)].seq.reverse_complement().translate(table=table)
+                        #print seq
+                    if '*' in seq[0:-1]:
+                        print 'changing again start and stop: %s %s' % (start, end+1)
+                        seq = seq_record[start:(end+1)].seq.reverse_complement().translate(table=table)
+                        #print seq
+                    if '*' in seq[0:-1]:
+                        print 'changing again start and stop: %s %s' % (start+2, end+1)
+                        seq = seq_record[(start+2):(end+1)].seq.reverse_complement().translate(table=table)
+                        #print seq
+
+                else:
+                    if "Mycoplasma" in seq_record.description:
+                        print 'Myco!'
+                        table=4
+                    print ncbi_accession, 'strand +', gene_name, start, end, table, seq_record.description
+                    seq = seq_record[start:end].seq.translate(table)
+
+                    if '*' in seq[0:-1] and table==1:
+                        print 'problem with translation, trying to identitfy translation table'
+                        handle = Entrez.efetch(db="nucleotide", id=ncbi_accession, rettype="gbwithparts", retmode="text")
+                        seq_record = SeqIO.read(handle, "genbank")
+                        for feature in seq_record.features:
+                            if feature.type == 'CDS':
+                                table = int(feature.qualifiers["transl_table"][0])
+                        seq = seq_record[start:end].seq.reverse_complement().translate(table=table)
+                        print ncbi_accession, 'strand -', gene_name, start, end, table, seq_record.description
+                        #print seq
+
+                    if '*' in seq[0:-1]:
+                        print 'changing start and stop: %s %s' % (start+1, end+1)
+                        seq = seq_record[(start+1):(end+1)].seq.translate(table=table)
+                        #print seq
+                    if '*' in seq[0:-1]:
+                        print 'changing again start and stop: %s %s' % (start, end+1)
+                        #print seq
+                        seq = seq_record[start:(end+1)].seq.translate(table=table)
+                    if '*' in seq[0:-1]:
+                        print 'changing again start and stop: %s %s' % (start+2, end+1)
+                        seq = seq_record[(start+2):(end+1)].seq.translate(table=table)
+
+                    if '*' in seq[0:-1]:
+                        print 'changing again start and stop: %s %s' % (start+3, end+2)
+                        seq = seq_record[(start+3):(end+2)].seq.translate(table=table)
+
+
+                if '*' in seq:
+                    seq = seq[0:-1]
+                if '*' in seq:
+                    print '############  could not translate sequence ##########', seq_record.description, start, end
+                    break
+
+
+                newrecord = SeqRecord(seq, id=gene_name, description=seq_record.description)
+
+                with open("all_seqs.fa", 'a') as f:
+                    SeqIO.write(newrecord, f, 'fasta')
+
+
+                handle.close()
+
+
+def parse_crisprfile(crisp_table):
+    accession2localisation = {}
+    with open(crisp_table, 'r') as f:
+        for line in f:
+            data = line.rstrip().split('\t')
+            if not '..' in data[1]:
+                continue
+            else:
+                if data[0] not in accession2localisation:
+                    accession2localisation[data[0]] = [data[1:4]]
+                else:
+                    accession2localisation[data[0]].append(data[1:4])
+    return accession2localisation
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a",'--accession',type=str,help="NCBI accession")
+    parser.add_argument("-t",'--crisp_table',type=str,help="crisp_table")
+
+
+    args = parser.parse_args()
+    get_protein_sequences(parse_crisprfile(args.crisp_table))
+    #get_genomic_data(args.accession)
+
+
+
--- eutils/taxid2genomes.py
+++ eutils/taxid2genomes.py
@@ -57,7 +57,230 @@ def get_complete_genomes_data(ncbi_taxon, complete = False):
         Organism = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['Organism']
         AssemblyStatus = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['AssemblyStatus']
         if complete:
-            if AssemblyStatus not in ['Complete Genome']:
+            if AssemblyStatus not in ['Complete Genome']: # , 'Chromosome'
+                print 'status:', AssemblyStatus
+                continue
+
+        NCBIReleaseDate = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['NCBIReleaseDate']
+        SpeciesName = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['SpeciesName']
+        SubmitterOrganization = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['SubmitterOrganization']
+        AssemblyName = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['AssemblyName']
+        RefSeq = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['Synonym']['RefSeq']
+        Genbank = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['Synonym']['Genbank']
+        Similarity = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['Synonym']['Similarity']
+        PartialGenomeRepresentation = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['PartialGenomeRepresentation']
+        Coverage = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['Coverage']
+
+        out_dir = local_dir+'/%s' % LastMajorReleaseAccession
+        try:
+            os.mkdir(out_dir)
+        except:
+            continue
+        # get ftp link in meta data string
+        print assembly_record['DocumentSummarySet']['DocumentSummary'][0]['Meta']
+        print assembly_record['DocumentSummarySet']['DocumentSummary'][0]['PropertyList']
+
+        if 'anomalous' in assembly_record['DocumentSummarySet']['DocumentSummary'][0]['PropertyList']:
+            anomalous = True
+        else:
+            anomalous = False
+
+        # check if RefSeq assembly was supressed
+        if 'suppressed_refseq' in assembly_record['DocumentSummarySet']['DocumentSummary'][0]['PropertyList']:
+            print 'Supressed RefSeq record, downloading GenBank record'
+            ftp_path = re.findall('<FtpPath type="GenBank">ftp[^<]*<', assembly_record['DocumentSummarySet']['DocumentSummary'][0]['Meta'])[0][50:-1]
+            get_ncbi_genome(ftp_path, out_dir)
+        elif 'latest' in assembly_record['DocumentSummarySet']['DocumentSummary'][0]['PropertyList']:
+            print 'Downloading RefSeq record'
+            try:
+                ftp_path = re.findall('<FtpPath type="RefSeq">ftp[^<]*<', assembly_record['DocumentSummarySet']['DocumentSummary'][0]['Meta'])[0][50:-1]
+                ftp_path = '/' + ftp_path
+                get_ncbi_genome(ftp_path, out_dir)
+            except IndexError:
+                print 'RefSeq link could not be identified, downloading GenBank record'
+                ftp_path = re.findall('<FtpPath type="GenBank">ftp[^<]*<', assembly_record['DocumentSummarySet']['DocumentSummary'][0]['Meta'])[0][50:-1]
+                get_ncbi_genome(ftp_path, out_dir)
+
+        # check if the assembly was replaced
+        elif 'replaced' in assembly_record['DocumentSummarySet']['DocumentSummary'][0]['PropertyList']:
+
+            i = int(LastMajorReleaseAccession.split('.')[1]) + 1
+            success = False
+            # identifiy new version of the assembly by increasing version number
+            while not success:
+                if i < 5:
+                    try:
+                        ftp_path = '/genomes/all/%s_%s' % (LastMajorReleaseAccession.split('.')[0] + '.%s' % i,
+                                                           AssemblyName)
+                        print ftp_path
+                        get_ncbi_genome(ftp_path, out_dir)
+
+                        success = True
+                    except:
+                        i+=i
+                else:
+                    print 'could not download assembly %s' % LastMajorReleaseAccession
+                    break
+        else:
+            'Unkown status for %s, not downloading' % LastMajorReleaseAccession
+        line = '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n' % (AssemblyName,
+                                                                             LastMajorReleaseAccession,
+                                                                             Taxid,
+                                                                             SpeciesName,
+                                                                             Organism,
+                                                                             AssemblyStatus,
+                                                                             NCBIReleaseDate,
+                                                                             SubmitterOrganization,
+                                                                             ftp_path,
+                                                                             PartialGenomeRepresentation,
+                                                                             Coverage,
+                                                                             RefSeq,
+                                                                             Genbank,
+                                                                             Similarity,
+                                                                             anomalous)
+
+        f.write(line.encode("utf-8"))
+    f.close()
+
+
+def get_sample_data(accession):
+
+    handle1 = Entrez.esearch(db="nuccore", term=accession)
+    record1 = Entrez.read(handle1)
+
+    ncbi_id = record1['IdList'][0]
+
+    handle2 = Entrez.elink(dbfrom="nuccore", db="biosample", id=ncbi_id)
+    record2 = Entrez.read(handle2)
+
+    id = record2[0]['LinkSetDb'][0]['Link'][0]['Id']
+
+    handle3 = Entrez.esummary(db='biosample',id=id, retmode='xml')
+    record = Entrez.read(handle3, validate=False)
+    for i in record['DocumentSummarySet']['DocumentSummary'][0]:
+        print "%s\t%s" % (i, record['DocumentSummarySet']['DocumentSummary'][0][i])
+
+
+def accession2assembly(accession):
+
+    handle1 = Entrez.esearch(db="nuccore", term=accession)
+    record1 = Entrez.read(handle1)
+
+    print record1
+
+    ncbi_id = record1['IdList'][-1]
+
+    print "ncbi_id", ncbi_id
+
+    handle = Entrez.elink(dbfrom="nuccore", db="assembly", id=ncbi_id)
+    record = Entrez.read(handle)
+
+    print record
+
+    f = open('ncbi_genome_download.log', 'w')
+    header = 'AssemblyName\tLastMajorReleaseAccession\tTaxid\tSpeciesName\tOrganism\t' \
+             'AssemblyStatus\tNCBIReleaseDate\tSubmitterOrganization\tftpPath\tPartialGenomeRepresentation\tCoverage\tRefSeq\tGenbank\tSimilarity\n'
+    f.write(header)
+    local_dir = os.getcwd()
+
+    handle_assembly = Entrez.esummary(db="assembly",id=record[0]['LinkSetDb'][0]['Link'][0]['Id'])
+    assembly_record = Entrez.read(handle_assembly)
+
+    LastMajorReleaseAccession = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['LastMajorReleaseAccession']
+    Taxid = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['Taxid']
+    Organism = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['Organism']
+    AssemblyStatus = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['AssemblyStatus']
+    NCBIReleaseDate = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['NCBIReleaseDate']
+    SpeciesName = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['SpeciesName']
+    SubmitterOrganization = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['SubmitterOrganization']
+    AssemblyName = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['AssemblyName']
+    RefSeq = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['Synonym']['RefSeq']
+    Genbank = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['Synonym']['Genbank']
+    Similarity = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['Synonym']['Similarity']
+    PartialGenomeRepresentation = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['PartialGenomeRepresentation']
+    Coverage = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['Coverage']
+
+    out_dir = local_dir+'/%s' % LastMajorReleaseAccession
+    os.mkdir(out_dir)
+
+    # get ftp link in meta data string
+    if 'latest' in assembly_record['DocumentSummarySet']['DocumentSummary'][0]['PropertyList']:
+        ftp_path = re.findall('<FtpPath type="GenBank">ftp[^<]*<', assembly_record['DocumentSummarySet']['DocumentSummary'][0]['Meta'])[0][50:-1]
+
+        get_ncbi_genome(ftp_path, out_dir)
+
+    elif 'replaced' in assembly_record['DocumentSummarySet']['DocumentSummary'][0]['PropertyList']:
+
+        i = int(LastMajorReleaseAccession.split('.')[1]) + 1
+        success = False
+        while not success:
+            if i < 5:
+                try:
+                    ftp_path = '/genomes/all/%s_%s' % (LastMajorReleaseAccession.split('.')[0] + '.%s' % i,
+                                                       AssemblyName)
+                    print ftp_path
+                    get_ncbi_genome(ftp_path, out_dir)
+
+                    success = True
+                except:
+                    i+=i
+            else:
+                print 'could not download assembly %s' % LastMajorReleaseAccession
+                break
+    else:
+        'Unkown status for %s, not downloading' % LastMajorReleaseAccession
+    line = '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n' % (AssemblyName,
+                                                                         LastMajorReleaseAccession,
+                                                                         Taxid,
+                                                                         SpeciesName,
+                                                                         Organism,
+                                                                         AssemblyStatus,
+                                                                         NCBIReleaseDate,
+                                                                         SubmitterOrganization,
+                                                                         ftp_path,
+                                                                         PartialGenomeRepresentation,
+                                                                         Coverage,
+                                                                         RefSeq,
+                                                                         Genbank,
+                                                                         Similarity)
+
+    f.write(line.encode("utf-8"))
+
+
+
+def bioproject2assemblies(accession, complete=False):
+
+    handle1 = Entrez.esearch(db="bioproject", term=accession)
+    record1 = Entrez.read(handle1)
+
+    ncbi_id = record1['IdList'][0]
+
+    handle = Entrez.elink(dbfrom="bioproject", db="assembly", id=ncbi_id)
+    record = Entrez.read(handle)
+
+    id_list = [i['Id'] for i in record[0]['LinkSetDb'][0]['Link']]
+    print id_list
+
+
+    local_dir = os.getcwd()
+    f = open('ncbi_genome_download.log', 'w')
+    header = 'AssemblyName\tLastMajorReleaseAccession\tTaxid\tSpeciesName\tOrganism\t' \
+             'AssemblyStatus\tNCBIReleaseDate\tSubmitterOrganization\tftpPath\tPartialGenomeRepresentation\tCoverage\tRefSeq\tGenbank\tSimilarity\tAnomalousAssembly\n'
+    f.write(header)
+
+    for i, one_assembly in enumerate(id_list):
+        print 'assembly %s (%s/%s)' % (one_assembly, i, len(id_list))
+        handle_assembly = Entrez.esummary(db="assembly",id=one_assembly)
+        assembly_record = Entrez.read(handle_assembly, validate=False)
+
+        LastMajorReleaseAccession = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['LastMajorReleaseAccession']
+
+        Taxid = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['Taxid']
+        Organism = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['Organism']
+        AssemblyStatus = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['AssemblyStatus']
+        if complete:
+            if AssemblyStatus not in ['Complete Genome', 'Chromosome']:
+                print 'status:', AssemblyStatus
                 continue
 
         NCBIReleaseDate = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['NCBIReleaseDate']
@@ -137,85 +360,6 @@ def get_complete_genomes_data(ncbi_taxon, complete = False):
                                                                              anomalous)
 
         f.write(line.encode("utf-8"))
-    f.close()
-
-def accession2assembly(accession):
-
-    handle1 = Entrez.esearch(db="nuccore", term=accession)
-    record1 = Entrez.read(handle1)
-
-    ncbi_id = record1['IdList'][0]
-
-    handle = Entrez.elink(dbfrom="nuccore", db="assembly", id=ncbi_id)
-    record = Entrez.read(handle)
-    f = open('ncbi_genome_download.log', 'w')
-    header = 'AssemblyName\tLastMajorReleaseAccession\tTaxid\tSpeciesName\tOrganism\t' \
-             'AssemblyStatus\tNCBIReleaseDate\tSubmitterOrganization\tftpPath\tPartialGenomeRepresentation\tCoverage\tRefSeq\tGenbank\tSimilarity\n'
-    f.write(header)
-    local_dir = os.getcwd()
-
-    handle_assembly = Entrez.esummary(db="assembly",id=record[0]['LinkSetDb'][0]['Link'][0]['Id'])
-    assembly_record = Entrez.read(handle_assembly)
-
-    LastMajorReleaseAccession = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['LastMajorReleaseAccession']
-    Taxid = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['Taxid']
-    Organism = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['Organism']
-    AssemblyStatus = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['AssemblyStatus']
-    NCBIReleaseDate = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['NCBIReleaseDate']
-    SpeciesName = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['SpeciesName']
-    SubmitterOrganization = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['SubmitterOrganization']
-    AssemblyName = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['AssemblyName']
-    RefSeq = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['Synonym']['RefSeq']
-    Genbank = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['Synonym']['Genbank']
-    Similarity = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['Synonym']['Similarity']
-    PartialGenomeRepresentation = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['PartialGenomeRepresentation']
-    Coverage = assembly_record['DocumentSummarySet']['DocumentSummary'][0]['Coverage']
-
-    out_dir = local_dir+'/%s' % LastMajorReleaseAccession
-    os.mkdir(out_dir)
-
-    # get ftp link in meta data string
-    if 'latest' in assembly_record['DocumentSummarySet']['DocumentSummary'][0]['PropertyList']:
-        ftp_path = re.findall('<FtpPath type="GenBank">ftp[^<]*<', assembly_record['DocumentSummarySet']['DocumentSummary'][0]['Meta'])[0][50:-1]
-
-        get_ncbi_genome(ftp_path, out_dir)
-
-    elif 'replaced' in assembly_record['DocumentSummarySet']['DocumentSummary'][0]['PropertyList']:
-
-        i = int(LastMajorReleaseAccession.split('.')[1]) + 1
-        success = False
-        while not success:
-            if i < 5:
-                try:
-                    ftp_path = '/genomes/all/%s_%s' % (LastMajorReleaseAccession.split('.')[0] + '.%s' % i,
-                                                       AssemblyName)
-                    print ftp_path
-                    get_ncbi_genome(ftp_path, out_dir)
-
-                    success = True
-                except:
-                    i+=i
-            else:
-                print 'could not download assembly %s' % LastMajorReleaseAccession
-                break
-    else:
-        'Unkown status for %s, not downloading' % LastMajorReleaseAccession
-    line = '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n' % (AssemblyName,
-                                                                         LastMajorReleaseAccession,
-                                                                         Taxid,
-                                                                         SpeciesName,
-                                                                         Organism,
-                                                                         AssemblyStatus,
-                                                                         NCBIReleaseDate,
-                                                                         SubmitterOrganization,
-                                                                         ftp_path,
-                                                                         PartialGenomeRepresentation,
-                                                                         Coverage,
-                                                                         RefSeq,
-                                                                         Genbank,
-                                                                         Similarity)
-
-    f.write(line.encode("utf-8"))
 
 
 if __name__ == '__main__':
@@ -224,11 +368,15 @@ if __name__ == '__main__':
     parser.add_argument("-t",'--taxon_id',type=str,help="get wgs link from taxonomic id")
     parser.add_argument("-a",'--accession',type=str,help="get assembly of a specific genbank/refseq accession")
     parser.add_argument("-c", '--complete', action="store_true", help="complete genome only (default = False)")
-
-
+    parser.add_argument("-s",'--sample',type=str,help="get sample data from accession id")
+    parser.add_argument("-b",'--bioproject',type=str,help="get all assemblies from bioproject")
 
     args = parser.parse_args()
     if args.taxon_id:
         get_complete_genomes_data(ncbi_taxon= args.taxon_id, complete = args.complete)
     if args.accession:
         accession2assembly(accession = args.accession)
+    if args.sample:
+        get_sample_data(accession=args.sample)
+    if args.bioproject:
+        bioproject2assemblies(accession=args.bioproject)
