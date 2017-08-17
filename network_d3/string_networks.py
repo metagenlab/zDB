36568364d9b9083bf22be0b47066d05a6c38a82c
Trestan Pillonel <trestan.pillonel@gmail.com>
2017-08-17 08:11:29 +0200

    neig_network switch from locus tags to seqfeature_ids

--- network_d3/string_networks.py
+++ network_d3/string_networks.py
@@ -7,17 +7,24 @@
 # ---------------------------------------------------------------------------
 
 
-def find_links_recusrsive(biodb, all_connected_groups, ratio_cutoff=0.5, n_comp_cutoff=1):
+def find_links_recusrsive(biodb, all_connected_seqfeatures, ratio_cutoff=0.5, n_comp_cutoff=1):
 
     import manipulate_biosqldb
     server, db = manipulate_biosqldb.load_db(biodb)
 
-    filter = '"' + '","'.join(all_connected_groups) + '"'
-    sql = 'select locus_1, locus_2 from interactions.colocalization_table_locus_%s where (locus_1 in (%s) or locus_2 in (%s)) and ' \
-          ' (ratio >= %s and n_comparisons >= %s)' % (biodb, filter, filter, ratio_cutoff, n_comp_cutoff)
+    if biodb == 'chlamydia_04_16':
+        filter = '"' + '","'.join(all_connected_seqfeatures) + '"'
 
-    print sql
+        sql = 'select locus_1, locus_2 from interactions.colocalization_table_locus_%s where (locus_1 in (%s) or locus_2 in (%s)) and ' \
+              ' (ratio >= %s and n_comparisons >= %s)' % (biodb, filter, filter, ratio_cutoff, n_comp_cutoff)
+    else:
+        all_connected_seqfeatures = [str(i) for i in all_connected_seqfeatures]
+        filter = ','.join(all_connected_seqfeatures)
 
+        sql = 'select locus_1, locus_2 from interactions.colocalization_table_locus_%s where (locus_1 in (%s) or locus_2 in (%s)) and ' \
+              ' (ratio >= %s and n_comparisons >= %s)' % (biodb, filter, filter, ratio_cutoff, n_comp_cutoff)
+
+        print sql
     data = server.adaptor.execute_and_fetchall(sql,)
 
     all_groups = []
@@ -26,7 +33,7 @@ def find_links_recusrsive(biodb, all_connected_groups, ratio_cutoff=0.5, n_comp_
             all_groups.append(i[0])
         if i[1] not in all_groups:
             all_groups.append(i[1])
-    if len(all_groups) > len(all_connected_groups):
+    if len(all_groups) > len(all_connected_seqfeatures):
         return find_links_recusrsive(biodb, all_groups, ratio_cutoff, n_comp_cutoff)
     else:
         return all_groups
@@ -42,7 +49,7 @@ def find_profile_links_recusrsive(biodb, all_connected_groups, max_euclidian_dis
     server, db = manipulate_biosqldb.load_db(biodb)
 
     filter = '"' + '","'.join(all_connected_groups) + '"'
-    sql = 'select * from comparative_tables.phylogenetic_profiles_euclidian_distance2_%s where (group_1 in (%s) or group_2 in (%s)) and ' \
+    sql = 'select * from comparative_tables.phylo_profiles_eucl_dist2_%s where (group_1 in (%s) or group_2 in (%s)) and ' \
           ' (euclidian_dist <= %s)' % (biodb, filter, filter, max_euclidian_dist)
 
     print sql
@@ -247,7 +254,7 @@ def generate_network(biodb,
     server, db = manipulate_biosqldb.load_db(biodb)
     filter = '"' + '","'.join(locus_tag_list) + '"'
     sql = 'select * from interactions.colocalization_table_locus_%s where (locus_1 in (%s) or locus_2 in (%s)) and ratio>=%s;' % (biodb, filter, filter, ratio_limit)
-
+    print sql
     data = server.adaptor.execute_and_fetchall(sql,)
 
     # filtering singletons
@@ -258,7 +265,7 @@ def generate_network(biodb,
         if i[1] not in locus_keep:
             locus_keep.append(i[1])
     # group_1    | group_2    | n_links | n_comparisons | ratio
-    #print locus_keep
+    print 'number of locus keep:', len(locus_keep)
     sources = '''
     <meta charset=utf-8 />
     <title>Visual style</title>
@@ -412,6 +419,16 @@ layout: {
     });
 
   });
+
+    $("#cy_png").click(function(e) {
+      var pngData = cy.png({maxWidth:3000,maxHeight:6000, full: true});
+
+    // put the png data in an img tag
+    $('#cy_png').attr('href', pngData);
+
+    });
+
+
 }); // on dom ready
 
 
@@ -462,6 +479,263 @@ layout: {
     return template % (width, height, ',\n'.join(node_list), ',\n'.join(edge_list), biodb)
 
 
+def generate_network_string(biodb,
+                     locus_tag_list,
+                     target_locus_list,
+                     taxon_id,
+                     ratio_limit,
+                     width=160,
+                     height=70,
+                     interpro=False,
+                     annot=False):
+    import manipulate_biosqldb
+    import re
+
+
+    server, db = manipulate_biosqldb.load_db(biodb)
+    filter = '"' + '","'.join(locus_tag_list) + '"'
+    sql = 'select t2.locus_tag,t3.locus_tag,t1.label_1,t1.label_2,t1.global_score ' \
+          ' from string.interactions_%s t1 inner join custom_tables.locus2seqfeature_id_%s t2 ' \
+          ' on t1.seqfeature_id_1=t2.seqfeature_id inner join custom_tables.locus2seqfeature_id_%s t3 ' \
+          ' on t1.seqfeature_id_2=t3.seqfeature_id where t1.taxon_id=%s and global_score>=%s and t2.locus_tag in (%s)' \
+          ' and t3.locus_tag in (%s) ;' % (biodb,
+                                                                                                  biodb,
+                                                                                                  biodb,
+                                                                                                  taxon_id,
+                                                                                                  ratio_limit,
+                                                                                                  filter,
+                                                                                                  filter,)
+
+    data = server.adaptor.execute_and_fetchall(sql,)
+
+    # filtering singletons
+    locus_keep = []
+    for i in data:
+        if i[0] not in locus_keep:
+            locus_keep.append(i[0])
+        if i[1] not in locus_keep:
+            locus_keep.append(i[1])
+    # 0 locus_tag
+    # 1 locus_tag
+    # 2 label_1
+    # 3 label_2
+    # 4 global_score
+    print 'number of locus keep:', len(locus_keep)
+    sources = '''
+    <meta charset=utf-8 />
+    <title>Visual style</title>
+    <meta name="viewport" content="user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, minimal-ui">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/4.10.3/css/bootstrap-slider.min.css">
+    <script src="http://marvl.infotech.monash.edu/webcola/cola.v3.min.js"></script>
+    <script src="cytoscape-cola.js"></script>
+    <link href="style.css" rel="stylesheet" />
+
+    '''
+
+    body = '''
+<body>
+  <div id="cy"></div>
+</body>
+    '''
+
+
+    # layout cola
+    template = '''
+
+<script>
+
+$(function(){ // on dom ready
+
+var cy = cytoscape({
+layout: {
+	name: 'cose',
+    maxSimulationTime: 6000,
+    randomize: true,
+    nodeSpacing: 30,
+    animate: true
+  },
+  container: document.getElementById('cy'),
+  boxSelectionEnabled: false,
+  autounselectify: true,
+
+  style: cytoscape.stylesheet()
+    .selector('node')
+      .css({
+        'shape': 'data(faveShape)',
+        'width': %s,
+	    'height': %s,
+        'content': 'data(name)',
+        'text-valign': 'center',
+        'text-outline-width': 0,
+        'text-outline-color': 'data(faveColor)',
+        'background-color': 'data(faveColor)',
+        'color': '#000000',
+	    'font-size': 18
+
+      })
+    .selector(':selected')
+      .css({
+        'border-width': 3,
+        'border-color': '#333'
+      })
+    .selector('edge')
+      .css({
+        'opacity': 0.666,
+        'width': 'data(strength)',
+        /*'target-arrow-shape': 'triangle',*/
+        /*'source-arrow-shape': 'circle',*/
+        'line-color': 'data(faveColor)',
+        'source-arrow-color': 'data(faveColor)',
+        'target-arrow-color': 'data(faveColor)',
+	'curve-style': 'bezier',
+        'control-point-distance': '20px',
+	'control-point-weight': '0.7', // '0': curve towards source node, '1': towards target node.
+
+      })
+    .selector('edge.questionable')
+      .css({
+        'line-style': 'dotted',
+        'target-arrow-shape': 'diamond'
+      })
+    .selector('.faded')
+      .css({
+        'opacity': 0.25,
+        'text-opacity': 0
+      }),
+
+  elements: {
+    nodes: [
+      %s
+    ],
+    edges: [
+      %s
+    ]
+  },
+
+
+  /*ready: function(){
+    window.cy = this;
+
+    // giddy up
+  }*/
+});
+
+
+
+  cy.$('node').forEach(function(n){
+    var g = n.data('annot');
+    var l = n.data('name');
+
+    n.qtip({
+      content: [
+        {
+          name: g,
+          url: '/chlamdb/locusx/%s/' + l + '/T'
+        }
+      ].map(function( link ){
+        return '<a target="_blank" href="' + link.url + '">' + link.name + '</a>';
+      }).join('<br />\\n'),
+      position: {
+        my: 'bottom left',
+        at: 'top right'
+      },
+      style: {
+
+        classes: 'qtip-bootstrap',
+        width: 180,
+        height: 130,
+        lineHeight: 1,
+        tip: {
+          width: 12,
+          height: 12,
+        }
+      }
+    });
+  });
+  cy.edges().forEach(function(n){
+    var g = n.data('n_comp');
+    var h = n.data('n_links');
+    n.qtip({
+      content: [
+        {
+          name: 'Num:' + g + " / " + h
+        },
+
+      ].map(function( link ){
+        return '<a target="_blank" href="' + link.url + '">' + link.name + '</a>';
+      }).join('<br />\\n'),
+      position: {
+        my: 'bottom left',
+        at: 'top right'
+      }
+    });
+
+  });
+
+    $("#cy_png").click(function(e) {
+      var pngData = cy.png({maxWidth:3000,maxHeight:6000, full: true});
+
+    // put the png data in an img tag
+    $('#cy_png').attr('href', pngData);
+
+    });
+
+
+}); // on dom ready
+
+
+
+</script>
+
+
+    '''
+
+    node_template1 = """{ data: { id: '%s', name: '%s', weight: 1, faveColor: '#%s', faveShape: 'ellipse' } }""" # 6FB1FC
+    node_template2 = """{ data: { id: '%s', name: '%s', weight: 1, faveColor: '#%s', faveShape: 'ellipse', annot: '%s' } }""" # 6FB1FC
+    edge_template = """{ data: { source: '%s', target: '%s', faveColor: '#%s', strength: %s , global_score:%s} }"""
+    #print interpro,interpro
+    node_list = []
+    for node in locus_keep:#locus_tag_list:
+        if node in target_locus_list:
+            color = "FA58F4"
+        else:
+            color="6FB1FC"
+        if annot:
+            #print interpro[node][0],
+            try:
+                node_annot = '%s<br>%s: %s' % (annot[node][1], interpro[node][0], interpro[node][1])
+            except:
+                node_annot = '%s<br>' % (annot[node][1])
+            node_annot = re.sub('\'', '', node_annot)
+            if 'hyp' in node_annot and not 'IPR' in node_annot:
+                color="F7BE81"
+            node_list.append(node_template2 % (node, node, color, node_annot))
+
+        else:
+            node_list.append(node_template1 % (node, node, "6FB1FC"))
+
+    edge_list = []
+    # 0 locus_tag
+    # 1 locus_tag
+    # 2 label_1
+    # 3 label_2
+    # 4 global_score
+    # """{ data: { source: '%s', target: '%s', faveColor: '#%s', strength: %s , global_score:%s} }"""
+    for edge in data:
+        #if float(edge[2])< 1:
+        edge_list.append(edge_template % (edge[0], edge[1], "47de47", (10-float(edge[4]))*2, edge[4]))
+
+        #else:
+        #   edge_list.append(edge_template % (edge[0], edge[1], "47de47", 10, edge[2], edge[3]))
+
+    return template % (width, height, ',\n'.join(node_list), ',\n'.join(edge_list), biodb)
+
+
+
+
 def generate_network_profile(biodb,
                              group_list,
                              reference_group_list,
@@ -473,7 +747,7 @@ def generate_network_profile(biodb,
     server, db = manipulate_biosqldb.load_db(biodb)
     filter = '"' + '","'.join(group_list) + '"'
     # group_1     | group_2     | euclidian_dist
-    sql = 'select * from comparative_tables.phylogenetic_profiles_euclidian_distance2_%s where (group_1 in (%s) and group_2 in (%s)) and euclidian_dist<=%s;' % (biodb, filter, filter, euclidian_distance_limit)
+    sql = 'select * from comparative_tables.phylo_profiles_eucl_dist2_%s where (group_1 in (%s) and group_2 in (%s)) and euclidian_dist<=%s;' % (biodb, filter, filter, euclidian_distance_limit)
     print sql
     data = server.adaptor.execute_and_fetchall(sql,)
 
@@ -651,6 +925,10 @@ layout: {
       }
     });
   });
+
+
+
+
 }); // on dom ready
 
 
