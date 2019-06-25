101c163d9802aeaeb94887ac7d00c14d8ddd6028
tpillone <trestan.pillonel@gmail.com>
2019-06-25 09:32:24 +0200

    updated load orthology script

--- db_setup/chlamdb-load-orthofinder.py
+++ db_setup/chlamdb-load-orthofinder.py
@@ -39,7 +39,9 @@ def add_orthogroup_to_seq(server, locus_tag2orthogroup, locus_tag2seqfeature_id_
     #| seqfeature_id | term_id | rank | value   |
     rank = 1
     term_id = add_orthogroup_term(server)
-    for locus_tag in locus_tag2orthogroup.keys():
+    for n, locus_tag in enumerate(locus_tag2orthogroup.keys()):
+        if n % 100 == 0:
+            print("%s / %s", n, len(locus_tag2orthogroup.keys()))
         seqfeature_id = locus_tag2seqfeature_id_dico[locus_tag]
         group = locus_tag2orthogroup[locus_tag]
         sql = 'INSERT INTO seqfeature_qualifier_value (seqfeature_id, term_id, rank, value) values (%s, %s, %s, "%s");' % (seqfeature_id,
