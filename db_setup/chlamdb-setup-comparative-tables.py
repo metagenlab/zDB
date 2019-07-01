0be50ba6201d08205d3c5864f0ec741b08177133
Trestan Pillonel <trestan.pillonel@gmail.com>
2019-07-01 10:14:21 +0200

    separated accession from taxid comparative tables setup

--- db_setup/chlamdb-setup-comparative-tables.py
+++ db_setup/chlamdb-setup-comparative-tables.py
@@ -709,55 +709,61 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    if args.orthology:
-        print("identity_closest_homolog")
-        identity_closest_homolog(args.database_name)
-        print("n_shared_orthogroup_table")
-        n_shared_orthogroup_table(args.database_name)
-        print("shared_orthogroups_average_identity")
-        shared_orthogroups_average_identity(args.database_name)
 
-        if args.accessions:
+
+    if args.accessions:
+        if args.orthology:
             create_comparative_tables_accession(args.database_name, "orthology")
             collect_orthogroup_accession(args.database_name)
 
-    if args.cog:
-        create_comparative_tables(args.database_name, "COG")
-        collect_COGs(args.database_name)
-
-        if args.accessions:
+        if args.cog:
             create_comparative_tables_accession(args.database_name, "COG")
             collect_COGs_accession(args.database_name)
 
 
-    if args.pfam:
-        create_comparative_tables(args.database_name, "Pfam")
-        collect_pfam(args.database_name)
-        if args.accessions:
+        if args.pfam:
             create_comparative_tables_accession(args.database_name, 'Pfam')
             collect_Pfam_accession(args.database_name)
 
-    if args.ec:
-
-        create_comparative_tables(args.database_name, "EC")
-        collect_EC(args.database_name)
-        if args.accessions:
+        if args.ec:
             create_comparative_tables_accession(args.database_name, "EC")
             collect_EC_accession(args.database_name)
 
-    if args.interpro:
-        create_comparative_tables(args.database_name, "interpro")
-        collect_interpro(args.database_name)
-        if args.accessions:
+        if args.interpro:
             create_comparative_tables_accession(args.database_name, "interpro")
             collect_interpro_accession(args.database_name)
 
 
-    if args.ko:
-        create_comparative_tables(args.database_name, "ko")
-        collect_ko(args.database_name)
-        if args.accessions:
+        if args.ko:
             create_comparative_tables_accession(args.database_name, "ko")
             collect_ko_accession(args.database_name)
 
-    #get_mysql_table("chlamydia_03_15", "Pfam")
+    if not if args.accessions:
+        if args.orthology:
+            print("identity_closest_homolog")
+            identity_closest_homolog(args.database_name)
+            print("n_shared_orthogroup_table")
+            n_shared_orthogroup_table(args.database_name)
+            print("shared_orthogroups_average_identity")
+            shared_orthogroups_average_identity(args.database_name)
+
+        if args.cog:
+            create_comparative_tables(args.database_name, "COG")
+            collect_COGs(args.database_name)
+
+        if args.pfam:
+            create_comparative_tables(args.database_name, "Pfam")
+            collect_pfam(args.database_name)
+
+        if args.ec:
+
+            create_comparative_tables(args.database_name, "EC")
+            collect_EC(args.database_name)
+
+        if args.interpro:
+            create_comparative_tables(args.database_name, "interpro")
+            collect_interpro(args.database_name)
+
+        if args.ko:
+            create_comparative_tables(args.database_name, "ko")
+            collect_ko(args.database_name)
