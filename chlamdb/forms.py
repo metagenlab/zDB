c7240b021e8bdbf5846d43596a478fb68b1f8042
tpillone <trestan.pillonel@gmail.com>
2019-07-16 01:27:59 +0200

    updated accession form: deal with identical decsriptions

--- chlamdb/forms.py
+++ chlamdb/forms.py
@@ -67,7 +67,9 @@ def get_accessions(database_name, all=False, plasmid=False):
         description = re.sub(", wgs", "", description)
         description = re.sub("Candidatus ", "", description)
         description = re.sub(".contig.0_1, whole genome shotgun sequence.", "", description)
-        accession_choices[i] = (accession[0], description)
+        #accession_choices[i] = (accession[0], description)
+        if description in accessions.values():
+            accessions += ' v2'
         accessions[accession[0]] = description
 
     accession_choices = []
