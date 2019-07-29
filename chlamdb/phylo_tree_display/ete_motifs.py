1c0af94f8a256fb559ccedbf02ae8b8c81272456
tpillone <trestan.pillonel@gmail.com>
2019-07-29 19:45:44 +0200

    left align TM tree header

--- chlamdb/phylo_tree_display/ete_motifs.py
+++ chlamdb/phylo_tree_display/ete_motifs.py
@@ -2314,8 +2314,6 @@ def draw_TM_tree(tree_name, locus2data):
             n.inner_background.color = "white"
             n.opacity = 1.
             n.rotation = -25
-            n.vt_align = 2
-            n.hz_align = 2
             ts.aligned_header.add_face(n, 1)
 
         locus_name = str(l)[3:len(str(l))]
