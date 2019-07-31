0ef1ac2ec5f2c1b583f9bf9a0b2308a3cd66b2dc
tpillone <trestan.pillonel@gmail.com>
2019-07-31 12:49:11 +0200

    deal with boolean uniprot reviewed/unreviewed

--- chlamdb/views.py
+++ chlamdb/views.py
@@ -2405,9 +2405,9 @@ def locusx(request, locus=None, menu=True):
             try:
                 uniprot_annotation = server.adaptor.execute_and_fetchall(sql12, )[0]
                 print(uniprot_annotation)
-                if uniprot_annotation[0] == 1:
+                if uniprot_annotation[0] == '1':
                     uniprot_annotation[0] == 'Reviewed'
-                if uniprot_annotation[0] == 0:
+                if uniprot_annotation[0] == '0':
                     uniprot_annotation[0] == 'Unreviewed'               
 
             except:
