8412be01dbbef1935347338fcce987a12d21226b
tpillone <trestan.pillonel@gmail.com>
2019-08-11 10:57:44 +0200

    plot species tree only if not present

--- chlamdb/views.py
+++ chlamdb/views.py
@@ -267,27 +267,32 @@ def home(request):
     sql = 'select taxon_id from bioentry where biodatabase_id=102 and accession in (%s)' % filter
     taxon_list = [str(i[0]) for i in server.adaptor.execute_and_fetchall(sql,)]
     '''
-    t1 = Tree(tree)
-    R = t1.get_midpoint_outgroup()
-    t1.set_outgroup(R)
-    '''
-    try:
-        t2 = t1.prune(taxon_list)
-    except:
-        pass
-    t1.ladderize()
-    '''
-    t, tss = phylo_tree_bar.plot_heat_tree(t1, biodb)
 
-    tss.show_branch_support = False
-    #t.render("test2.svg", tree_style=ts)
-    path = settings.BASE_DIR + '/assets/temp/tree.svg'
-    asset_path = '/temp/tree.svg'
+    path = settings.BASE_DIR + '/assets/temp/species_tree.svg'
+    asset_path = '/temp/species_tree.svg'
 
-    t.render(path, dpi=500, tree_style=tss)
+    # plot phylo only of not already in assets
+    if not os.path.exists(settings.BASE_DIR + '/assets/temp/species_tree.svg'):
+        
+        t1 = Tree(tree)
+        R = t1.get_midpoint_outgroup()
+        t1.set_outgroup(R)
+        '''
+        try:
+            t2 = t1.prune(taxon_list)
+        except:
+            pass
+        t1.ladderize()
+        '''
+        t, tss = phylo_tree_bar.plot_heat_tree(t1, biodb)
 
-    sql_n_genomes = 'select count(*) from (select distinct taxon_id from bioentry t1 inner join biodatabase t2 on t1.biodatabase_id=t2.biodatabase_id where t2.name="%s") A;' % biodb
-    n_genomes = server.adaptor.execute_and_fetchall(sql_n_genomes,)[0][0]
+        tss.show_branch_support = False
+        #t.render("test2.svg", tree_style=ts)
+
+        t.render(path, dpi=500, tree_style=tss)
+
+        sql_n_genomes = 'select count(*) from (select distinct taxon_id from bioentry t1 inner join biodatabase t2 on t1.biodatabase_id=t2.biodatabase_id where t2.name="%s") A;' % biodb
+        n_genomes = server.adaptor.execute_and_fetchall(sql_n_genomes,)[0][0]
 
     return render(request, 'chlamdb/home.html', locals())
 
