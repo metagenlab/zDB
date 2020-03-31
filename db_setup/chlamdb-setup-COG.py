e75053b247726d87ca6f5d15cd9c0187966b08b1
tpillone <trestan.pillonel@gmail.com>
2020-03-31 20:25:32 +0200

    download COG reference fasta from NCBI ftp, compatibility with sqlite setup

--- db_setup/chlamdb-setup-COG.py
+++ db_setup/chlamdb-setup-COG.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
 
 
-def parse_table(table_path):
-    with 
-
-
 def create_COG_tables(biodb):
     from chlamdb.biosqldb import manipulate_biosqldb
     server, db = manipulate_biosqldb.load_db(biodb)
@@ -12,37 +8,29 @@ def create_COG_tables(biodb):
     cursor = server.adaptor.cursor
 
 
-
-    sql1 = 'CREATE TABLE COG_cog_2014 (domain_id varchar(100),' \
-                            ' genome_name varchar(200),' \
-                            ' protein_id INT,' \
-                            ' protein_length INT,' \
-                            ' domain_start INT,' \
-                            ' domain_end INT,' \
-                            ' COG_id INT,' \
-                            ' membership_class INT,' \
-                            ' index COG_id(COG_id),' \
-                            ' index protein_id(protein_id))'
-
-    sql2 = 'CREATE table COG_cog_names_2014 (COG_id INT,' \
-                                 ' COG_name varchar(100),' \
-                                 ' description varchar(200),' \
-                                 ' index COG_id(COG_id))'
+    sql2 = 'CREATE table COG_cog_names_2014 (COG_id INTEGER PRIMARY KEY,' \
+                                            ' COG_name varchar(100),' \
+                                            ' description varchar(200))'
 
     sql3 = 'CREATE table COG_cog_id2cog_category (COG_id INT,' \
-                                 ' category_id int,' \
-                                 ' index COG_id(COG_id),' \
-                                 ' index category_id(category_id))'
+           ' category_id int)'
 
 
     sql4 = 'create table COG_code2category (code varchar(5),' \
                                            ' description TEXT,' \
-                                           ' category_id INT unsigned primary KEY AUTO_INCREMENT);'
+                                           ' category_id INTEGER primary KEY);'
+                                           
+    sql_index2 = 'create index cciccid on COG_cog_id2cog_category(COG_id)'
+    sql_index3 = 'create index ccicccid on COG_cog_id2cog_category(category_id)'
 
-    cursor.execute(sql1,)
+
     cursor.execute(sql2,)
     cursor.execute(sql3,)
     cursor.execute(sql4,)
+    
+    cursor.execute(sql_index2,)
+    cursor.execute(sql_index3,)
+    
     conn.commit()
 
 def COG_id2cog_data_from_website(GOG_id):
@@ -58,9 +46,9 @@ def COG_id2cog_data_from_website(GOG_id):
     category = title.text.split('[')[1].split(']')[0]
     return [description, category]
 
+
 def load_cog_tables(biodb,
                     cognames_2014, 
-                    cog_2014, 
                     cog_categories):
     '''
 
@@ -85,18 +73,25 @@ def load_cog_tables(biodb,
     conn = server.adaptor.conn
     cursor = server.adaptor.cursor
     
-    conn.set_character_set('utf8')
-    cursor.execute('SET NAMES utf8;')
-    cursor.execute('SET CHARACTER SET utf8;')
-    cursor.execute('SET character_set_connection=utf8;')
+    
+    try:
+        conn.set_character_set('utf8')
+        cursor.execute('SET NAMES utf8;')
+        cursor.execute('SET CHARACTER SET utf8;')
+        cursor.execute('SET character_set_connection=utf8;')
+    except:
+        cursor.execute('PRAGMA encoding="UTF-8";')
 
-    with open(cog_categories, 'r') as f:
-        sql = 'insert into COG_code2category (code, description) values (%s, %s)'
-        for row in f:
-            if row[0] == '#':
-                continue
-            data = row.rstrip().split('\t')
-            cursor.execute(sql, (data[0], data[1]))
+    sql = 'insert into COG_code2category (code, description) values ("%s", "%s")'
+    for row in cog_categories:
+        if len(row) == 0:
+            continue
+        if row[0] == '#':
+            continue
+        data = row.split('\t')
+        sql_str = sql % (data[0], data[1])
+        print(sql_str)
+        cursor.execute(sql_str, )
     conn.commit()
 
     sql2 = 'select code, category_id from COG_code2category'
@@ -107,118 +102,28 @@ def load_cog_tables(biodb,
     cursor.execute(sql3,)
     cog_description2cog_category_id = manipulate_biosqldb.to_dict(cursor.fetchall())
 
-    with open(cog_categories, 'r') as f:
-        for row in f:
-            if row[0] == '#':
-                continue
-            data = row.rstrip().split('\t')
-            sql = 'insert into code2categories'
-    n=0
-    with open(cognames_2014, 'rU', encoding='windows-1252') as f:
-        for row in f:
-            data = row.rstrip().split('\t')
-            if data[0][0] == '#':
-                continue
+    for row in cognames_2014:
+        if len(row) == 0:
+            continue
+        data = row.split('\t')
+        if data[0][0] == '#':
+            continue
 
-            # split multiple function and insert one row/function
-            fonctions = list(data[1])
-            for fonction in fonctions:
-                sql = 'insert into COG_cog_id2cog_category values (%s, %s)' % (n,
-                                                                               cog_category2cog_category_id[fonction])
-                cursor.execute(sql,)
+        # split multiple function and insert one row/function
+        fonctions = list(data[1])
 
-            sql = 'insert into COG_cog_names_2014(COG_id, COG_name, description) values(%s,"%s","%s")' % (n,
-                                                                                                          data[0],
-                                                                                                          re.sub('"','',data[2]))
+        sql = 'insert into COG_cog_names_2014(COG_name, description) values("%s","%s")' % (data[0],
+                                                                                           re.sub('"','',data[2]))
+        cursor.execute(sql,)
+        
+        cog_id = cursor.lastrowid
+        
+        for fonction in fonctions:
+            sql = 'insert into COG_cog_id2cog_category values (%s, %s)' % (cog_id,
+                                                                           cog_category2cog_category_id[fonction])
             cursor.execute(sql,)
-            n+=1
+
     conn.commit()
-
-    sql = 'select COG_name, COG_id from COG_cog_names_2014'
-
-    cursor.execute(sql,)
-    cog_name2cog_id = manipulate_biosqldb.to_dict(cursor.fetchall())
-
-    problem = []
-    with open(cog_2014, 'r') as f:
-        f = [i for i in f]
-        for n, row in enumerate(f):
-            if n%100 ==0:
-                print ('%s / %s' % (n, len(f)))
-            data = row.rstrip().split(',')
-            if data[0][0] == '#':
-                continue
-            try:
-                sql = 'insert into COG_cog_2014(COG_id, ' \
-                      ' domain_id, ' \
-                      ' genome_name, ' \
-                      ' protein_id,' \
-                      ' protein_length,' \
-                      ' domain_start,' \
-                      ' domain_end,' \
-                      ' membership_class' \
-                      ' ) values(%s,%s,"%s", %s, %s, %s, %s, %s)' % (cog_name2cog_id[data[6]],
-                                                                         data[0],
-                                                                         data[1],
-                                                                         data[2],
-                                                                         data[3],
-                                                                         data[4],
-                                                                         data[5],
-                                                                         data[7])
-
-                cursor.execute(sql,)
-            except:
-                # cog missing from the cognames table
-                # fetch data from the NCBI website and insert new COG name entry
-                # mail to Yuri Wolf, should not happen again (they cleaned tables from "old" removed COGs)
-                print ('problem with COG id %s' % data[6])
-                cog_web_data = COG_id2cog_data_from_website(data[6])
-                description = cog_web_data[0]
-                categories = cog_web_data[1]
-
-                # get last COG id
-                sql = 'select COG_id from COG_cog_names_2014 order by COG_id DESC limit 1'
-                cursor.execute(sql,)
-                COG_id = int(cursor.fetchall()[0][0]) + 1
-                cog_name2cog_id[data[6]] = COG_id
-                for cat in cog_description2cog_category_id:
-                    if cat in categories:
-                        print ('category present!', cat)
-                        category_id = cog_description2cog_category_id[cat]
-                        sql = 'insert into COG_cog_id2cog_category values (%s, %s)' % (COG_id,
-                                                                                       category_id)
-                        cursor.execute(sql,)
-                        conn.commit()
-
-                # insert COG name
-                sql = 'insert into COG_cog_names_2014(COG_id, COG_name, description) values(%s,"%s","%s")' % (cog_name2cog_id[data[6]],
-                                                                                                              data[6],
-                                                                                                              description)
-                cursor.execute(sql,)
-                conn.commit()
-
-                # insert entry
-                sql = 'insert into COG_cog_2014(COG_id, ' \
-                      ' domain_id, ' \
-                      ' genome_name, ' \
-                      ' protein_id,' \
-                      ' protein_length,' \
-                      ' domain_start,' \
-                      ' domain_end,' \
-                      ' membership_class' \
-                      ' ) values(%s,%s,"%s", %s, %s, %s, %s, %s)' % (COG_id,
-                                                                     data[0],
-                                                                     data[1],
-                                                                     data[2],
-                                                                     data[3],
-                                                                     data[4],
-                                                                     data[5],
-                                                                     data[7])
-
-                cursor.execute(sql,)
-    conn.commit()
-    print ('number of problems:', len(problem))
-
 
 if __name__ == '__main__':
     import argparse
@@ -227,32 +132,37 @@ if __name__ == '__main__':
     
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", '--cognames_2014', type=str, help="cognames2003-2014.tab")
-    parser.add_argument("-c", '--cog_2014', type=str, help="cog2003-2014.csv")
     parser.add_argument("-f", '--functions_2014', type=str, help="fun2003-2014.tab")
-    parser.add_argument("-d", '--biodb', type=str, help="Biodb name")
+    parser.add_argument("-b", '--biodb', type=str, help="Biodb name")
+    parser.add_argument("-d", '--download', action='store_true', help="Download from NCBI FTP")
     
 
     args = parser.parse_args()
+    
     create_COG_tables(args.biodb)
     
     cognames = 'ftp://ftp.ncbi.nlm.nih.gov/pub/COG/COG2014/data/cognames2003-2014.tab'
-    cog_2014 = 'ftp://ftp.ncbi.nlm.nih.gov/pub/COG/COG2014/data/cog2003-2014.csv'
     cog_functions = 'ftp://ftp.ncbi.nlm.nih.gov/pub/COG/COG2014/data/fun2003-2014.tab'
     
     if args.download:
         print("Downloading ftp://ftp.ncbi.nlm.nih.gov/pub/COG/COG2014/data/cognames2003-2014.tab")
-        cog_functions_data = urllib.request.Request(cog_functions).read().decode('utf-8').split('\n')
-        print(cog_functions_data)
+        cog_functions_data = urllib.request.urlopen(cog_functions).read().decode('utf-8').split('\n')
+
         print("Downloading ftp://ftp.ncbi.nlm.nih.gov/pub/COG/COG2014/data/cognames2003-2014.tab...")
-        cognames_data = urllib.request.Request(cognames).read().decode('utf-8').split('\n')
-        print("Downloading... ftp://ftp.ncbi.nlm.nih.gov/pub/COG/COG2014/data/fun2003-2014.tab")
-        cog_2014_data = urllib.request.Request(cog_2014).read().decode('utf-8').split('\n')
+        cognames_data = urllib.request.urlopen(cognames).read().decode('windows-1252').split('\n')
+        
+        print("done")
+        print(cog_functions_data)
         load_cog_tables(args.biodb,
-                        download=True)
+                        cognames_data,
+                        cog_functions_data)
 
     else:
+        with open(args.cognames_2014, 'r') as f:
+            cognames_2014 = [i.rstrip() for i in f]
+        with open(args.functions_2014, 'r') as f:
+            functions_2014 = [i.rstrip() for i in f]
+                  
         load_cog_tables(args.biodb,
-                        args.cognames_2014, 
-                        args.cog_2014, 
-                        args.functions_2014, 
-                        download=False)
+                        cognames_2014, 
+                        functions_2014)
