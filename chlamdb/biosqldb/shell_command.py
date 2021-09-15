83acd3584c42f9ac5b4af9f526ca3efcefdfc051
bkm <bastian.marquis@unil.ch>
2021-09-15 14:46:45 +0200

    Removed biosqldb

--- chlamdb/biosqldb/shell_command.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-def shell_command(command):
-    """execute shell command and return std output and std error"""
-    process = subprocess.Popen(command, stdout = subprocess.PIPE,
-                                stderr = subprocess.PIPE, shell = True)
-    stdout_str, stderr_str = process.communicate()
-    return(stdout_str.decode(), stderr_str.decode(), process.returncode)
