b2241236894d9b9ce0cc37086e26726bbdaa7351
bastian <bastian.marquis@chuv.ch>
2022-09-21 10:26:57 +0200

    Tentative fix for the versioning problem

--- conda/recipe/build.sh
+++ conda/recipe/build.sh
@@ -5,7 +5,7 @@ ZDB_DIR="${PREFIX}/share/${ZDB}"
 
 mkdir -p ${PREFIX}/bin ${ZDB_DIR}
 
-sed "s/nextflow_dir_val/${ZDB_DIR//\//\\/}/" bin/zdb > ${PREFIX}/bin/zdb
+sed "s/nextflow_dir_val/${ZDB_DIR}/" bin/zdb > ${PREFIX}/bin/zdb
 chmod u+x ${PREFIX}/bin/zdb
 
 mv bin/ annotation_pipeline.nf nextflow.config db_setup.nf zdb/  docs/ FAQ.txt README.md ${ZDB_DIR}
