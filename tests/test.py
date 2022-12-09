2b1f66c9e0f23e2c02043f25c0689fdec72f4cb8
bastian <bastian.marquis@chuv.ch>
2022-12-09 12:29:00 +0100

    Unit test framework, WIP

---
+++ tests/test.py
@@ -0,0 +1,52 @@
+
+import os
+import shutil
+
+import annotations
+
+class TestCheckGbk:
+    def __init__(self, test_name, test_dir, run, check):
+        self.name = test_name
+        self.test_dir = test_dir
+        self.passed = False
+        self.run = run
+
+    def cleanup(self):
+        shutil.remove(self.test_dir)
+
+
+def check_gbk_run_test_1(test):
+    annotations.check_gbk(test.test_dir + "/test.csv")
+
+def check_gbk_run_test_1(test):
+    annotations.check_gbk(test.test_dir + "/test.csv")
+
+
+tests_list = [
+    TestCheckGbk("Locus tag collision", "test1"),
+    TestCheckGbk("Organism collision", "test2"),
+    TestCheckGbk("Different organism in same gbk", "test3"),
+    TestCheckGbk("Organism renaming", "test4"),
+    TestCheckGbk("Wrong format", "test5")
+]
+
+
+failed = []
+n_tests = 0
+for test in tests_list:
+    test.run(test)
+
+    if not test.passed:
+        failed.append(test)
+    n_tests += 1
+
+if len(failed) == 0:
+    print(f"All tests passed ({n_tests}/{n_tests})")
+else:
+    print(f"{len(failed)}/{n_tests} tests have failed:")
+    for failed_test in failed:
+        print(failed_test.name)
+
+print("Cleaning up")
+for test in tests_list:
+    test.cleanup()
