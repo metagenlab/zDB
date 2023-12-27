0cd5a688e1d53459b74b5c2b6e23bdc0357b7f5c
Niklaus Johner <niklaus.johner@a3.epfl.ch>
2023-12-27 14:21:38 +0100

    Add annotation pipeline tests.

--- testing/pipelines/test_db_setup.py
+++ testing/pipelines/test_db_setup.py
@@ -1,48 +1,20 @@
 import glob
 import os
 import tempfile
-from unittest import skip
 
-import nextflow
-from testing.base import BaseTestCase
+from testing.base import BasePipelineTestCase
 
 
-class TestDBSetupPipeline(BaseTestCase):
+class TestDBSetupPipeline(BasePipelineTestCase):
 
-    def setUp(self):
-        super(TestDBSetupPipeline, self).setUp()
-        basedir = os.path.dirname(os.path.dirname(os.path.dirname(
-            os.path.abspath(__file__))))
-        self.nf_file_path = os.path.join(basedir, "db_setup.nf")
-        self.db_dir = tempfile.TemporaryDirectory()
-        self.singularity_dir = tempfile.TemporaryDirectory()
-        # self.nf_dir = tempfile.TemporaryDirectory()
+    nf_filename = "db_setup.nf"
+    _ref_db_dir = None
 
-        self.nf_params = {
-            "docker": "false",
-            "conda": "false",
-            "singularity": "true",
-            "base_db": self.db_dir.name,
-            "singularity_dir": self.singularity_dir.name,
-        }
-        self.nf_args = {}
-        # self.nf_args = {"output_path": self.nf_dir.name}
-
-    def execute_pipeline(self):
-        return nextflow.run(self.nf_file_path,
-                            params=self.nf_params,
-                            **self.nf_args)
-
-    def assert_success(self, execution):
-        self.assertEqual(execution.status, "OK",
-                         "Pipeline execution failed")
-        self.assertEqual(execution.return_code, "0",
-                         "Pipeline execution failed")
-        for proc in execution.process_executions:
-            self.assertEqual(proc.return_code, "0",
-                             "Process execution failed")
-            self.assertEqual(proc.status, "COMPLETED",
-                             "Process execution failed")
+    @property
+    def ref_db_dir(self):
+        if self._ref_db_dir is None:
+            self._ref_db_dir = tempfile.TemporaryDirectory()
+        return self._ref_db_dir.name
 
     def assert_created_files(self, proc, files):
         created_files = os.listdir(proc.path)
@@ -74,7 +46,7 @@ class TestDBSetupPipeline(BaseTestCase):
         # Files are copied to db directory
         self.assertItemsEqual(
             expected_files,
-            os.listdir(os.path.join(self.db_dir.name, "pfam")))
+            os.listdir(os.path.join(self.ref_db_dir, "pfam")))
 
     def test_creating_cog_db(self):
         self.nf_params["cog"] = "true"
@@ -90,7 +62,6 @@ class TestDBSetupPipeline(BaseTestCase):
         smp_files = glob.glob(os.path.join(download_process.path, "COG*.smp"))
         self.assertTrue(len(smp_files) > 1000)
 
-        setup_process = execution.process_executions[1]
         expected_files = ["cdd_to_cog", "cog_db.aux", "cog_db.freq",
                           "cog_db.loo", "cog_db.phr", "cog_db.pin",
                           "cog_db.psi", "cog_db.psq", "cog_db.rps",
@@ -98,7 +69,7 @@ class TestDBSetupPipeline(BaseTestCase):
         # Files are moved to db directory
         self.assertItemsEqual(
             expected_files,
-            os.listdir(os.path.join(self.db_dir.name, "cog")))
+            os.listdir(os.path.join(self.ref_db_dir, "cog")))
 
     def test_creating_ko_db(self):
         self.nf_params["ko"] = "true"
@@ -109,7 +80,7 @@ class TestDBSetupPipeline(BaseTestCase):
                          ["setup_ko_db:download_ko_profiles"])
 
         # Files are moved to db directory
-        db_dir = os.path.join(self.db_dir.name, "kegg")
+        db_dir = os.path.join(self.ref_db_dir, "kegg")
         self.assertTrue(os.path.isfile(os.path.join(db_dir, "ko_list")))
         self.assertTrue(os.path.isfile(
             os.path.join(db_dir, "profiles", "prokaryote.hal")))
@@ -128,7 +99,6 @@ class TestDBSetupPipeline(BaseTestCase):
         download_process = execution.process_executions[0]
         self.assert_created_files(download_process, ["swissprot.fasta"])
 
-        setup_process = execution.process_executions[1]
         # Files are moved to db directory
         expected_files = ['swissprot.fasta.phr',
                           'swissprot.fasta',
@@ -137,4 +107,4 @@ class TestDBSetupPipeline(BaseTestCase):
 
         self.assertItemsEqual(
             expected_files,
-            os.listdir(os.path.join(self.db_dir.name, "uniprot", "swissprot")))
+            os.listdir(os.path.join(self.ref_db_dir, "uniprot", "swissprot")))
