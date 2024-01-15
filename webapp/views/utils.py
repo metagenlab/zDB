d65f8ce8bdfe3bb78b6f1183187f3a001128fb20
Niklaus Johner <niklaus.johner@a3.epfl.ch>
2024-01-15 16:29:45 +0100

    Move page2title to views.utils.

--- webapp/views/utils.py
+++ webapp/views/utils.py
@@ -2,3 +2,42 @@ def safe_replace(string, search_string, replace_string):
     if string:
         return string.replace(search_string, replace_string)
     return string
+
+
+title2page = {
+    'COG Ortholog': ['fam_cog'],
+    'Comparisons: Antimicrobial Resistance': [
+        'amr_comparison', 'index_comp_amr', 'entry_list_amr', 'extract_amr'],
+    'Comparisons: Clusters of Orthologous groups (COGs)': [
+        'cog_barchart', 'index_comp_cog', 'cog_phylo_heatmap',
+        'cog_comparison', 'entry_list_cog', 'extract_cog', 'heatmap_COG',
+        'pan_genome_cog', 'plot_heatmap_cog', 'venn_cog'],
+    'Comparisons: Kegg Orthologs (KO)': [
+        'entry_list_ko', 'extract_ko', 'heatmap_ko', 'index_comp_ko',
+        'ko_comparison', 'module_barchart', 'pan_genome_ko', 'plot_heatmap_ko',
+        'venn_ko'],
+    'Comparisons: PFAM domains': [
+        'entry_list_pfam', 'extract_pfam', 'heatmap_pfam', 'pan_genome_pfam',
+        'pfam_comparison', 'venn_pfam', 'index_comp_pfam', 'plot_heatmap_pfam'],
+    'Comparisons: orthologous groups': [
+        'extract_orthogroup', 'heatmap_orthogroup', 'index_comp_orthogroup',
+        'orthogroup_comparison', 'pan_genome_orthogroup', 'plot_heatmap_orthogroup',
+        'venn_orthogroup'],
+    'Genome alignments: Circos plot': ['circos'],
+    'Genome alignments: Plot region': ['plot_region'],
+    'Genomes: table of contents': ['extract_contigs', 'genomes_intro'],
+    'Homology search: Blast': ['blast'],
+    'Kegg Ortholog': ['fam_ko'],
+    'Kegg metabolic pathways': ['kegg_genomes'],
+    'Kegg module': ['KEGG_module_map'],
+    'Metabolism: kegg based': [
+        'KEGG_mapp_ko', 'kegg', 'kegg_genomes_modules', 'kegg_module',
+        'kegg_module_subcat', 'module_comparison'],
+    'Pfam domain': ['fam_pfam'],
+    'Phylogeny': ['phylogeny_intro'],
+    'Table of content: Genomes': ['genomes'],
+}
+
+page2title = {}
+for value, keys in title2page.items():
+    page2title.update({key: value for key in keys})
--- webapp/views/views.py
+++ webapp/views/views.py
@@ -12,6 +12,7 @@ from io import StringIO
 from tempfile import NamedTemporaryFile
 
 import bibtexparser
+import chlamdb.circosjs as circosjs
 import matplotlib.colors as mpl_col
 import numpy as np
 import pandas as pd
@@ -25,6 +26,11 @@ from Bio.Blast.Applications import (NcbiblastnCommandline,
 from Bio.Seq import Seq
 from Bio.SeqFeature import FeatureLocation, SeqFeature
 from Bio.SeqRecord import SeqRecord
+from chlamdb.forms import (make_blast_form, make_circos_form,
+                           make_extract_form, make_metabo_from,
+                           make_module_overview_form,
+                           make_pathway_overview_form, make_plot_form,
+                           make_single_genome_form, make_venn_from)
 from django.conf import settings
 from django.http import JsonResponse
 from django.shortcuts import render
@@ -37,13 +43,7 @@ from lib.ete_phylo import (Column, EteTree, KOAndCompleteness,
 from lib.KO_module import ModuleParser
 from reportlab.lib import colors
 
-import chlamdb.circosjs as circosjs
-from chlamdb.forms import (make_blast_form, make_circos_form,
-                           make_extract_form, make_metabo_from,
-                           make_module_overview_form,
-                           make_pathway_overview_form, make_plot_form,
-                           make_single_genome_form, make_venn_from)
-from chlamdb.utils import safe_replace
+from views.utils import page2title, safe_replace
 
 # could also be extended to cache the results of frequent queries
 # (e.g. taxid -> organism name) to avoid db queries
@@ -53,44 +53,6 @@ with DB.load_db(settings.BIODB_DB_PATH, settings.BIODB_CONF) as db:
                        (mandatory, value) in hsh_config.items() if not mandatory}
     missing_mandatory = [name for name, (mandatory, value) in hsh_config.items()
                          if mandatory and not value]
-
-title2page = {
-    'COG Ortholog': ['fam_cog'],
-    'Comparisons: Antimicrobial Resistance': [
-        'amr_comparison', 'index_comp_amr', 'entry_list_amr', 'extract_amr'],
-    'Comparisons: Clusters of Orthologous groups (COGs)': [
-        'cog_barchart', 'index_comp_cog', 'cog_phylo_heatmap',
-        'cog_comparison', 'entry_list_cog', 'extract_cog', 'heatmap_COG',
-        'pan_genome_cog', 'plot_heatmap_cog', 'venn_cog'],
-    'Comparisons: Kegg Orthologs (KO)': [
-        'entry_list_ko', 'extract_ko', 'heatmap_ko', 'index_comp_ko',
-        'ko_comparison', 'module_barchart', 'pan_genome_ko', 'plot_heatmap_ko',
-        'venn_ko'],
-    'Comparisons: PFAM domains': [
-        'entry_list_pfam', 'extract_pfam', 'heatmap_pfam', 'pan_genome_pfam',
-        'pfam_comparison', 'venn_pfam', 'index_comp_pfam', 'plot_heatmap_pfam'],
-    'Comparisons: orthologous groups': [
-        'extract_orthogroup', 'heatmap_orthogroup', 'index_comp_orthogroup',
-        'orthogroup_comparison', 'pan_genome_orthogroup', 'plot_heatmap_orthogroup',
-        'venn_orthogroup'],
-    'Genome alignments: Circos plot': ['circos'],
-    'Genome alignments: Plot region': ['plot_region'],
-    'Genomes: table of contents': ['extract_contigs', 'genomes_intro'],
-    'Homology search: Blast': ['blast'],
-    'Kegg Ortholog': ['fam_ko'],
-    'Kegg metabolic pathways': ['kegg_genomes'],
-    'Kegg module': ['KEGG_module_map'],
-    'Metabolism: kegg based': [
-        'KEGG_mapp_ko', 'kegg', 'kegg_genomes_modules', 'kegg_module',
-        'kegg_module_subcat', 'module_comparison'],
-    'Pfam domain': ['fam_pfam'],
-    'Phylogeny': ['phylogeny_intro'],
-    'Table of content: Genomes': ['genomes'],
-}
-
-page2title = {}
-for value, keys in title2page.items():
-    page2title.update({key: value for key in keys})
 
 
 def my_locals(local_dico):
