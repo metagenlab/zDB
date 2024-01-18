693849bd0a3b1b4b1668445743cc67e4a6514e4b
Niklaus Johner <niklaus.johner@a3.epfl.ch>
2024-01-18 16:19:40 +0100

    Add accumulation/rarefaction box to AMR comparison index view.

--- webapp/views/views.py
+++ webapp/views/views.py
@@ -177,10 +177,7 @@ class ComparisonIndexView(View, ComparisonViewMixin):
     def boxes(self):
         boxes = ["entry-list", "extraction", "venn",
                  "tabular-comparison", "heatmap", "accumulation-rarefaction"]
-        if self.comp_type == "amr":
-            boxes = ["entry-list", "extraction", "venn",
-                     "tabular-comparison", "heatmap"]
-        elif self.comp_type == "orthogroup":
+        if self.comp_type == "orthogroup":
             boxes.remove("entry-list")
         elif self.comp_type == "ko":
             boxes.append("barcharts")
