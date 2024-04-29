2053181bcaf958ae69bc7d2c732a46c47186f18f
Niklaus Johner <niklaus.johner@a3.epfl.ch>
2024-04-29 15:17:41 +0200

    Add dependency to django autocomplete light.

--- webapp/settings/settings.py
+++ webapp/settings/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dal',
+    'dal_select2',
     'chlamdb',
     'gunicorn',
     'templatetags',
