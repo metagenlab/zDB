7f6810dc50da71bb698942c21fe31ee1494633c5
Niklaus Johner <niklaus.johner@a3.epfl.ch>
2024-05-02 08:53:31 +0200

    Handle missing arguments in autocomplete views.

--- webapp/views/autocomplete.py
+++ webapp/views/autocomplete.py
@@ -6,9 +6,9 @@ from views.utils import AccessionFieldHandler
 class AutocompleteTaxid(Select2ListView):
 
     def get_list(self):
-        with_plasmids = self.forwarded["include_plasmids"]
-        exclude = self.forwarded["exclude"]
-        exclude_taxids_in_groups = self.forwarded["exclude_taxids_in_groups"]
+        with_plasmids = self.forwarded.get("include_plasmids", False)
+        exclude = self.forwarded.get("exclude", [])
+        exclude_taxids_in_groups = self.forwarded.get("exclude_taxids_in_groups", [])
         return AccessionFieldHandler().get_choices(
             with_plasmids=with_plasmids,
             exclude=exclude,
@@ -19,7 +19,9 @@ class AutocompleteNMissing(Select2ListView):
 
     def get_list(self):
         taxids, plasmids = AccessionFieldHandler().extract_choices(
-            self.forwarded["included"], self.forwarded["include_plasmids"])
-        n_max = len(taxids) + len(plasmids)
+            self.forwarded.get("included", []), self.forwarded.get("include_plasmids", False))
+        n_max = len(taxids)
+        if plasmids:
+            n_max += len(plasmids)
         choices = [(i, i) for i in range(n_max)]
         return choices
