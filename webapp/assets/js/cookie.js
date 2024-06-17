c500a0c3ff7f5ee8ec3b25ccc10586d8ae51193e
Niklaus Johner <niklaus.johner@a3.epfl.ch>
2024-06-17 08:33:11 +0200

    Move getCookie function to its own js module.

---
+++ webapp/assets/js/cookie.js
@@ -0,0 +1,8 @@
+function getCookie(name)
+  {
+    const regex = new RegExp(`(^| )${name}=([^;]+)`)
+    const match = document.cookie.match(regex)
+    if (match) {
+      return match[2]
+    }
+  }
