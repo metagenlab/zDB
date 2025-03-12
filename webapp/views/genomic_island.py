63fa4aa2b7606184664297ea4e1c428ad18e1501
Niklaus Johner <niklaus.johner@a3.epfl.ch>
2025-03-12 14:11:54 +0100

    Add representation of genomic island to detail view.

--- webapp/views/genomic_island.py
+++ webapp/views/genomic_island.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views import View
 from views.mixins import BaseViewMixin
 from views.utils import format_genome
+from views.utils import genomic_region_df_to_js
+from views.utils import locusx_genomic_region
 
 
 class GenomicIsland(BaseViewMixin, View):
@@ -12,11 +14,25 @@ class GenomicIsland(BaseViewMixin, View):
         self.gis_id, self.bioentry_id, self.start_pos, self.end_pos = (
             self.db.get_genomic_island(entry_id)
         )
+        all_infos, wd_start, wd_end, contig_size, contig_topology = (
+            locusx_genomic_region(
+                self.db,
+                bioentry=self.bioentry_id,
+                window_start=self.start_pos,
+                window_stop=self.end_pos,
+            )
+        )
+        genomic_region = genomic_region_df_to_js(
+            all_infos, wd_start, wd_end, contig_size, contig_topology
+        )
+        window_size = wd_end - wd_start
         context = self.get_context(
             entry_id=self.gis_id,
             start_pos=self.start_pos,
             end_pos=self.end_pos,
             description=self.description,
+            genomic_region=genomic_region,
+            window_size=window_size,
         )
         return render(request, self.template, context)
 
