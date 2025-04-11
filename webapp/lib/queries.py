13a96be248433590cac56d9160edd4b44060020f
Niklaus Johner <niklaus.johner@a3.epfl.ch>
2025-04-11 16:37:19 +0200

    Fix get_containing_genomic_islands query.
    The query was broken when changing the GiViewMixin to deal with
    GI clusters instead of GIs.

--- webapp/lib/queries.py
+++ webapp/lib/queries.py
@@ -162,7 +162,7 @@ class GIQueries(BaseQueries):
     ]
 
     def get_containing_genomic_islands(self, bioentry_id, start, stop):
-        sql = f"SELECT {self.id_col}, start_pos, end_pos FROM {self.description_table} WHERE bioentry_id=? AND (? BETWEEN start_pos AND end_pos OR ? BETWEEN start_pos AND end_pos)"
+        sql = f"SELECT {self.id_col}, start_pos, end_pos FROM {self.hit_table} WHERE bioentry_id=? AND (? BETWEEN start_pos AND end_pos OR ? BETWEEN start_pos AND end_pos)"
         return self.server.adaptor.execute_and_fetchall(sql, [bioentry_id, start, stop])
 
     def get_gi_descriptions(self, gis_ids):
